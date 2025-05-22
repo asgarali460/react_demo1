976ee56d233e41354a67d82b82aa61f533c2462d
nazmeen parween <asgarali460@gmail.com>
2025-05-22 12:50:58 +0530

    first commit

---
+++ o1vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Test from "./Demo"
+function App() {
+
+  return (
+    <>
+      <h1>Welcome</h1>
+      <Test/>
+    </>
+  )
+}
+
+export default App
---
+++ o1vitereact/src/Demo.jsx
@@ -0,0 +1,8 @@
+function Test() {
+
+  return (
+    <h1>React</h1>
+  )
+}
+
+export default Test
